49d5d78fc60653a975dd711c12982111db7daa70
rasim-mehtijev <rasim.mehtijev@insly.com>
2023-02-16 20:40:12 +0200

    api, useEffect

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,41 +1,48 @@
+import { useState, useEffect } from 'react';
 import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
+import { getPriceData } from '../services/apiService';
+import ErrorModal from '../ErrorModal';
+import moment from 'moment';
 
 function Body() {
 
-    const data = [
-        {
-            name: 'Page A',
-            pv: 2400,
-            uv: 4000,
-        },
-        {
-            name: 'Page B',
-            pv: 1400,
-            uv: 3000,
-        },
-        {
-            name: 'Page C',
-            pv: 9800,
-            uv: 2000,
-        },
-        {
-            name: 'Page D',
-            pv: 3900,
-            uv: 2800,
-        }
-    ]
+    const [data, setData] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        getPriceData()
+            .then(({ success, data, messages}) => {
+
+                if (!success) {
+                    throw messages[0];
+                }
+                
+                const newData = data.ee.map(d => {
+                    return {
+                        price: d.price,
+                        hour: moment.unix(d.timestamp).hours(),
+                    }
+                });
+
+                setData(newData);
+            })
+            .catch((error) => setErrorMessage(error.toString()));
+    }, []);
+
     return (
-        <ResponsiveContainer width="100%" height={400} >
-            <LineChart data={data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-        </ResponsiveContainer>
+        <>
+            <ResponsiveContainer width="100%" height={400} >
+                <LineChart data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="hour" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="price" stroke="#8884d8" />
+                </LineChart>
+            </ResponsiveContainer>
+            <ErrorModal errorMessage={errorMessage} handleClose={() => setErrorMessage(null)} />
+        </>
     );
 }
 
---
+++ src/services/apiService.js
@@ -0,0 +1,16 @@
+import moment from 'moment';
+
+const apiUrl = 'https://dashboard.elering.ee/api';
+
+export async function getPriceData() {
+    const start = moment().subtract(10, 'hours').format();
+    const end = moment().add(30, 'hours').format();
+
+    const params = new URLSearchParams({
+        start,
+        end,
+    });
+
+    const response = await fetch(`${apiUrl}/nps/price?${params}`);
+    return response.json();
+}
